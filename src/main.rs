1fda0086620bbf103d25bf44d0356cb9946d3dbd
MrAliSalehi <alisalehi498@yahoo.com>
2022-12-16 17:08:28 +0330

    finished
    Signed-off-by: MrAliSalehi <alisalehi498@yahoo.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,53 @@
+#![allow(non_snake_case)]
+extern crate walkdir;
+
+use std::env;
+use std::path::{Path, PathBuf};
+
+use colored::Colorize;
+use walkdir::{DirEntry, WalkDir};
 
 fn main() {
-    println!("Hello, world!");
+    let mut subtitlesInDir = Vec::new();
+    let currentDir = env::current_dir().unwrap();
+    GetSubtitlesInDirectory(&currentDir, &mut subtitlesInDir);
+
+    println!("{} '{}'", format!("Current Dir:").green(), currentDir.to_str().unwrap());
+
+    for subtitle in subtitlesInDir {
+        MoveSubtitle(subtitle);
+    }
+}
+
+fn MoveSubtitle(subtitle: DirEntry) {
+    let subtitlePathBuf = PathBuf::from(subtitle.path());
+
+    let parent = subtitlePathBuf.parent().expect("could not find the parent directory.");
+    let subtitleDirectoryInParentDir = format!("{}/subs", parent.to_str().unwrap());
+
+    if !Path::exists(Path::new(&subtitleDirectoryInParentDir)) {
+        std::fs::create_dir(&subtitleDirectoryInParentDir).expect("could not create directory in parent Dir.");
+    }
+    let fileName = subtitlePathBuf.file_name().unwrap().to_str().unwrap();
+
+    let subtitleFullPath = subtitlePathBuf.to_str().unwrap();
+
+    let errCopyFile = format!("cant copy the file:[{}]", fileName).red();
+    std::fs::copy(&subtitleFullPath, format!("{}/{}", &subtitleDirectoryInParentDir, fileName))
+        .expect(&errCopyFile);
+
+    let errRemoveFile = format!("could not remove the subtitle: {}", &subtitleFullPath).red();
+    std::fs::remove_file(&subtitleFullPath)
+        .expect(&errRemoveFile);
+}
+
+
+fn GetSubtitlesInDirectory(dir: &PathBuf, outList: &mut Vec<DirEntry>) {
+    for file in WalkDir::new(dir)
+        .into_iter().filter_map(|f| f.ok()).filter(|f| f.metadata().unwrap().is_file()) {
+        let fileName = file.file_name().to_str().unwrap();
+        if fileName.ends_with(".vtt") || fileName.ends_with(".srt") {
+            outList.push(file);
+        }
+    }
 }
