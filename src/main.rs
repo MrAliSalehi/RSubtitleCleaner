acc059c21834c6ac84a9053218ad3771cf8467bb
MrAliSalehi <alisalehi498@yahoo.com>
2022-12-17 07:14:34 +0330

    added docs
    Signed-off-by: MrAliSalehi <alisalehi498@yahoo.com>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ use std::path::{Path, PathBuf};
 use colored::Colorize;
 use walkdir::{DirEntry, WalkDir};
 
+/// starting point of the application.
+///
+/// get the [starting path](GetStartingPath).
+///
+/// get [all subtitles in that directory recursively](GetSubtitlesInDirectory).
+///
+/// **foreach** subtitle found in given directory, call [`MoveSubtitle`] function and pass dawn the subtitle [path](DirEntry)
 fn main() {
     let currentDir = GetStartingPath();
 
@@ -20,9 +27,12 @@ fn main() {
     }
 }
 
+///
+/// this will try to get the **starting path** _(the path that program will start to scanning for subtitles)_.
+///  - if it doesnt get any argument, it will use the [**current directory**](env::current_dir()) `(root directory of the application)`
+///  - pass an argument like this : `./SubtitleCleaner /path/to/clean/`
 fn GetStartingPath() -> String {
     let arg = env::args().nth(1);
-
     let currentDir;
     if let Some(..) = arg {
         currentDir = env::current_dir().unwrap().to_str().unwrap().to_string();
@@ -32,6 +42,11 @@ fn GetStartingPath() -> String {
     currentDir
 }
 
+/// this will [walk](walkdir::WalkDir) through the given directory(`dir` argument) **recursively**.
+/// - fetch all the files with these formats:
+/// - - *.vtt
+/// - - *.srt
+/// **returns** a [`Vec<DirEntry>`] of all found subtitles.
 fn GetSubtitlesInDirectory(dir: &str, outList: &mut Vec<DirEntry>) {
     for file in WalkDir::new(dir)
         .into_iter().filter_map(|f| f.ok()).filter(|f| f.metadata().unwrap().is_file()) {
@@ -42,6 +57,12 @@ fn GetSubtitlesInDirectory(dir: &str, outList: &mut Vec<DirEntry>) {
     }
 }
 
+/// this will move the given subtitle(from path) to a sub-directory called **subs** in the parent directory.
+///  - find the parent dir, consider this file:(`/path/to/my_subtitle1.srt`)
+///  - - parent will be `/path/to`
+///  - - **IF** it doesnt exists: **create** a directory in parent directory called **subs** : `/path/to/subs`
+///  - - **copy** the subtitle to new directory : `/path/to/subs/my_subtitle1.srt`
+///  - - **remove** the old file : rm `/path/to/my_subtitle1.srt`
 fn MoveSubtitle(subtitle: DirEntry) {
     let subtitlePathBuf = PathBuf::from(subtitle.path());
 
@@ -63,6 +84,3 @@ fn MoveSubtitle(subtitle: DirEntry) {
     std::fs::remove_file(subtitleFullPath)
         .expect(&errRemoveFile);
 }
-
-
-
